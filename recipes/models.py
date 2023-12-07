7c80c9508820564a2e1850a8b2e8545acfdee393
Matheus Gabriel <matheusgabriel.d.barros@gmail.com>
2023-12-07 14:45:26 -0300

    change models category and user

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
+       
+
+
+
 class Recipe(models.Model):
     title = models.CharField( max_length=65)
     description = models.CharField( max_length=165)
@@ -15,3 +24,12 @@ class Recipe(models.Model):
     updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d', height_field=None, width_field=None, max_length=None)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
